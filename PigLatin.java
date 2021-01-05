05d1211a937b1aa1d9777fb8218c2b2074e9e055
Elizabeth Stansberry <estansberry20@stuy.edu>
2021-01-04 20:42:20 -0700

    Done

--- PigLatin.java
+++ PigLatin.java
@@ -2,10 +2,24 @@ import java.util.*;
 import java.lang.*;
 public class PigLatin{
   public static void main(String args[]){
-    System.out.println(pigLatinBest("4chan"));
+    Scanner linescan = new Scanner(System.in);
+    String inputtext;
+    String wordtext;
+    String outputtext = "";
+    while(linescan.hasNextLine()){
+      inputtext = linescan.nextLine();
+      outputtext = "";
+      Scanner wordscan = new Scanner(inputtext);
+      while(wordscan.hasNext()){
+        wordtext = wordscan.next();
+        wordtext = pigLatinBest(wordtext);
+        outputtext = outputtext + wordtext + " ";
+      } System.out.println(outputtext);
+    }
   }
   public static String pigLatinSimple(String s){
     String vowels = "a, e, i, o, u";
+    s = s.toLowerCase();
     if(vowels.indexOf(s.substring(0,1)) != -1){
       s = s + "hay";
     } else{
@@ -16,6 +30,7 @@ public class PigLatin{
   public static String pigLatin(String s){
     String vowels = "a, e, i, o, u";
     String digraphs = "bl ,  br ,  ch ,  ck ,  cl ,  cr ,  dr ,  fl ,  fr ,  gh ,  gl ,  gr ,  ng ,  ph ,  pl ,  pr ,  qu ,  sc ,  sh ,  sk ,  sl ,  sm ,  sn ,  sp ,  st ,  sw ,  th ,  tr ,  tw ,  wh ,  wr";
+    s = s.toLowerCase();
     if(vowels.indexOf(s.substring(0,1)) != -1){
       s = s + "hay";
     }else if(digraphs.indexOf(s.substring(0,2)) != -1){
@@ -29,6 +44,7 @@ public class PigLatin{
     String vowels = "a, e, i, o, u";
     String digraphs = "bl ,  br ,  ch ,  ck ,  cl ,  cr ,  dr ,  fl ,  fr ,  gh ,  gl ,  gr ,  ng ,  ph ,  pl ,  pr ,  qu ,  sc ,  sh ,  sk ,  sl ,  sm ,  sn ,  sp ,  st ,  sw ,  th ,  tr ,  tw ,  wh ,  wr";
     String extra = "";
+    s = s.toLowerCase();
     if(!(Character.isLetterOrDigit(s.charAt(s.length() - 1)))){
       extra = s.substring(s.length() - 1);
       s = s.substring(0, s.length() - 1);
