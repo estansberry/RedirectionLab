83049160abf21e1eb650157573e60c2eabb42a8f
Elizabeth Stansberry <estansberry20@stuy.edu>
2021-01-04 20:27:02 -0700

    Done with best

--- PigLatin.java
+++ PigLatin.java
@@ -1,7 +1,8 @@
 import java.util.*;
+import java.lang.*;
 public class PigLatin{
   public static void main(String args[]){
-    System.out.println(pigLatin("check"));
+    System.out.println(pigLatinBest("4chan"));
   }
   public static String pigLatinSimple(String s){
     String vowels = "a, e, i, o, u";
@@ -22,6 +23,26 @@ public class PigLatin{
     }else{
       s = s.substring(1 , s.length()) + s.charAt(0) + "ay";
     }return(s);
+  }
+
+  public static String pigLatinBest(String s){
+    String vowels = "a, e, i, o, u";
+    String digraphs = "bl ,  br ,  ch ,  ck ,  cl ,  cr ,  dr ,  fl ,  fr ,  gh ,  gl ,  gr ,  ng ,  ph ,  pl ,  pr ,  qu ,  sc ,  sh ,  sk ,  sl ,  sm ,  sn ,  sp ,  st ,  sw ,  th ,  tr ,  tw ,  wh ,  wr";
+    String extra = "";
+    if(!(Character.isLetterOrDigit(s.charAt(s.length() - 1)))){
+      extra = s.substring(s.length() - 1);
+      s = s.substring(0, s.length() - 1);
+    }if(!(Character.isLetter(s.charAt(0)))){
+      return(s);
+    }
+    if(vowels.indexOf(s.substring(0,1)) != -1){
+      s = s + "hay";
+    }else if(digraphs.indexOf(s.substring(0,2)) != -1){
+      s = s.substring(2, s.length()) + s.substring(0,2) + "ay";
+    }else{
+      s = s.substring(1 , s.length()) + s.charAt(0) + "ay";
+    }s = s + extra;
+    return(s);
   }
 
 }
