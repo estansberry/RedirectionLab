e439143159635f73b959372deb61251ffed10f9c
Elizabeth Stansberry <estansberry20@stuy.edu>
2021-01-04 17:23:32 -0700

    Simple done

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,20 @@
+import java.util.*;
 public class PigLatin{
+  public static void main(String args[]){
+    System.out.println(pigLatinSimple("alphabet"));
+  }
   public static String pigLatinSimple(String s){
-    
+    ArrayList<String> vowels = new ArrayList<String>() {
+              {add("a");
+                add("e");
+                add("i");
+                add("o");
+                add("u");}
+              };
+    if(vowels.contains(s.substring(0,1))){
+      s = s + "hay";
+    } else{
+      s = s.substring(1 , s.length()) + s.charAt(0) + "ay";
+    }return(s);
   }
 }
